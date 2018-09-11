9a85423c07b59c82f89a0cacc314046db22d191b
Giovanni Jacobsen <superpieman157@yahoo.com>
2018-09-11 09:16:53 -0500

    Made a raise calculator

---
+++ chapter3/src/tutorial/DemoRaise.java
@@ -0,0 +1,25 @@
+package tutorial;
+
+public class DemoRaise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        double salary = 200.00;
+        double startingWage = 800.00;
+        
+        System.out.println("Demonstrating some raises");
+        
+        predictRaise(400.00);
+        predictRaise(salary);
+        predictRaise(startingWage);
+	}
+	
+	public static void predictRaise(double salary)
+	{
+		double newSalary;
+		final double RAISE_RATE = 1.10;
+		newSalary = salary * RAISE_RATE;
+		System.out.println("Current salary: " + salary + " After raise: " + newSalary);
+	}
+
+}
---
+++ chapter3/src/youDoIt/paradiseInfo.java
@@ -0,0 +1,16 @@
+package youDoIt;
+
+public class paradiseInfo {
+
+	public static void main(String[] args)
+		{
+        	displayInfo();
+		}
+	    public static void displayInfo()
+	    {
+		
+        System.out.println("Paradise spa wants to apmper you.");
+        System.out.println("We will make you look good.");
+	}
+
+}
